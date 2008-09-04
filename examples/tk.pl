1d0b348395a8c2bb9184445e0dd8c0ccfd8e73fa
brian d foy <brian@stonehenge.com>
2008-09-04 09:10:41 -0700

    * Starting to hook up the variables to the entries

--- examples/tk.pl
+++ examples/tk.pl
@@ -3,81 +3,105 @@ use warnings;
 
 use Tk;
 
+my $Vars = {};
 
-my $mw = MainWindow->new( );
-$mw->resizable( 0, 0 );
-$mw->title( 'BackPAN Indexer 1.00' );
-my $menubar = _menubar( $mw );
-
-my( $top, $middle, $bottom ) = map {
-	$mw->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );
-	} 1 .. 3;
-
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-my $tracker = _make_frame( $top, 'left' );
-
-my $tracker_left        = $tracker->Frame->pack( -anchor => 'w', -side => 'left' );
-foreach my $label ( qw(Total Done Left Errors ) )
-	{
-	my $frame = $tracker_left->Frame->pack( -side => 'top' );
-	$frame->Label( -text => $label, -width => 6 )->pack( -side => 'left' );
-	$frame->Entry( -width => 6 )->pack( -side => 'right' );
-	}
-
-my $tracker_right        = $tracker->Frame->pack( -anchor => 'w', -side => 'left'  );
-foreach my $label ( qw(Total Done Left Errors ) )
-	{
-	my $frame = $tracker_right->Frame->pack( -side => 'top' );
-	$frame->Label( -text => $label, -width => 6 )->pack( -side => 'left' );
-	$frame->Entry( -width => 6 )->pack( -side => 'right' );
-	}
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-my @recent = qw( a b c d e );
-my $jobs    = $middle->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );;
-
-my $count_frame = _make_frame( $jobs, 'left' );
-$count_frame->Label( -text => '#',          -width =>  3 )->pack( -side => 'top' );
-$count_frame->Listbox(
-	-height => 5,
-	-width  => 3,
-	-listvariable => [ 1 .. 5 ],
-	)->pack( -side => 'bottom');
-	
-my $pid_frame  = _make_frame( $jobs, 'left' );
-$pid_frame->Label( -text => 'PID',        -width =>  6 )->pack( -side => 'top' );
-$pid_frame->Listbox(
-	-height => 5,
-	-width  => 6,
-	-listvariable => [ map { int rand 65535 } 1 .. 5 ],
-	)->pack( -side => 'bottom');
-
-my $proc_frame = $jobs->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );
-$proc_frame->Label( -text => 'Processing', -width => 35 )->pack( -side => 'top' );
-$proc_frame->Listbox(
-	-height => 5,
-	-listvariable => \ @recent,
-	)->pack( -side => 'bottom', -expand => 1, -fill => 'x');
-
-
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-my @errors = qw( dog bird cat );
-my $errors  = $bottom->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );
-$errors->Label( -text => 'Errors', )->pack( -side => 'top', -anchor => 'w');
-$errors->Listbox(
-	-height => 10,
-	-listvariable => \ @recent,
-	)->pack(
-		-expand => 1,
-		-fill => 'x',
-		-side => 'left',
-		-anchor => 'w',
-			);
+my $mw = do_tk_stuff( $Vars);
+$mw->repeat( 1_000, \ &the_steak );
 
 MainLoop;
+
+
+sub the_steak
+	{
+	
+	
+	
+	$Vars->{total}++;
+	
+	}
+
+
+sub do_tk_stuff 
+	{
+	my $mw = MainWindow->new;
+	
+	$mw->resizable( 0, 0 );
+	$mw->title( 'BackPAN Indexer 1.00' );
+	my $menubar = _menubar( $mw );
+	
+	my( $top, $middle, $bottom ) = map {
+		$mw->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );
+		} 1 .. 3;
+	
+	
+	
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	my $tracker = _make_frame( $top, 'left' );
+	
+	my $tracker_left        = $tracker->Frame->pack( -anchor => 'w', -side => 'left' );
+	foreach my $label ( qw(Total Done Left Errors ) )
+		{
+		my $frame = $tracker_left->Frame->pack( -side => 'top' );
+		$frame->Label( -text => $label, -width => 6 )->pack( -side => 'left' );
+		$frame->Entry( -width => 6, -textvariable => \ $Vars->{total}, -relief => 'flat' )->pack( -side => 'right' );
+		}
+	
+	my $tracker_right        = $tracker->Frame->pack( -anchor => 'w', -side => 'left'  );
+	foreach my $label ( qw(Total Done Left Errors ) )
+		{
+		my $frame = $tracker_right->Frame->pack( -side => 'top' );
+		$frame->Label( -text => $label, -width => 6 )->pack( -side => 'left' );
+		$frame->Entry( -width => 6 )->pack( -side => 'right' );
+		}
+	
+	
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	my @recent = qw( a b c d e );
+	my $jobs    = $middle->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );;
+	
+	my $count_frame = _make_frame( $jobs, 'left' );
+	$count_frame->Label( -text => '#',          -width =>  3 )->pack( -side => 'top' );
+	$count_frame->Listbox(
+		-height => 5,
+		-width  => 3,
+		-listvariable => [ 1 .. 5 ],
+		)->pack( -side => 'bottom');
+		
+	my $pid_frame  = _make_frame( $jobs, 'left' );
+	$pid_frame->Label( -text => 'PID',        -width =>  6 )->pack( -side => 'top' );
+	$pid_frame->Listbox(
+		-height => 5,
+		-width  => 6,
+		-listvariable => [ map { int rand 65535 } 1 .. 5 ],
+		)->pack( -side => 'bottom');
+	
+	my $proc_frame = $jobs->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );
+	$proc_frame->Label( -text => 'Processing', -width => 35 )->pack( -side => 'top' );
+	$proc_frame->Listbox(
+		-height => 5,
+		-listvariable => \ @recent,
+		)->pack( -side => 'bottom', -expand => 1, -fill => 'x');
+	
+	
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	my @errors = qw( dog bird cat );
+	my $errors  = $bottom->Frame->pack( -anchor => 'w', -expand => 1, -fill => 'x' );
+	$errors->Label( -text => 'Errors', )->pack( -side => 'top', -anchor => 'w');
+	$errors->Listbox(
+		-height => 10,
+		-listvariable => \ @recent,
+		)->pack(
+			-expand => 1,
+			-fill => 'x',
+			-side => 'left',
+			-anchor => 'w',
+				);
+				
+				
+	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+	return $mw;
+	}
+	
 
 sub _make_frame
 	{
