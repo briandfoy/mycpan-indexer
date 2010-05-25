10d803f43ae79dcb744e3c031f14e6c2419bfd2b
brian d foy <brian.d.foy@gmail.com>
2010-05-25 12:51:35 -0500

    * Extract the MD5s for all the files in the distro

--- examine_inc_md5/extract_md5s.pl
+++ examine_inc_md5/extract_md5s.pl
@@ -37,20 +37,49 @@ foreach my $dir ( @dirs )
 		foreach my $module ( @{ $yaml->{dist_info}{module_info} } )
 			{
 			no warnings 'uninitialized';
+				
 			my $version = $module->{version_info}{value};
 			if( $version =~ /[^0-9_.]/ )
 				{
 				my $hex = unpack 'H*', $version;
 				warn "Strange version in $file for $module->{primary_package}: [$version|$hex]\n";
-				$version;
 				}
 
-			say join "|", 
-				@{ $module }{ qw(md5 bytesize primary_package) },
-				$version,
-				$dist_file;
+			write_line(
+				{
+				version   => $version || '',
+				blib      => 1,
+				dist_file => $dist_file,
+				map {; $_ => $module->{$_} } qw(md5 name bytesize primary_package)
+				}
+				);
+			}
+
+		foreach my $file ( @{ $yaml->{dist_info}{manifest_file_info} } )
+			{
+			no warnings 'uninitialized';
+			next if $file->{name} =~ m<(^|/)\.(git|svn)/>;
+			
+			write_line(
+				{
+				version         => '',
+				blib            => 0,
+				dist_file       => $dist_file,
+				primary_package => '',
+				map {; $_ => $file->{$_} } qw(name md5 bytesize)
+				}
+				);
 			}
 
 		}
 
+	}
+
+sub write_line
+	{
+	my( $hash ) = shift;
+	no warnings 'uninitialized';
+	
+	say join "|",
+		@{ $hash }{ qw(md5 name blib bytesize primary_package version dist_file) };	
 	}
