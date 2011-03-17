ecadc574e8d10918eeacc2bce9c870c10e9d0934
brian d foy <brian.d.foy@gmail.com>
2011-03-16 20:15:42 -0700

    Match column names in md5 database match data file names

--- examine_inc_md5/backpan_md5.sql
+++ examine_inc_md5/backpan_md5.sql
@@ -1,7 +1,9 @@
 CREATE TABLE backpan_md5 (
-	md5       text,
-	bytesize  int,
-	namespace text,
-	version   text,
-	path      text
+	md5             text,
+	filename        text,
+	blib            int,
+	bytesize        int,
+	primary_package text,
+	version         text,
+	dist_file       text
 	);
