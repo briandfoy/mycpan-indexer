be663b8e37d3374b377979acea3527d8fb1f4495
brian d foy <brian.d.foy@gmail.com>
2015-01-03 17:05:28 -0500

    Update copyright statement

--- lib/MyCPAN/Indexer/Queue.pm
+++ lib/MyCPAN/Indexer/Queue.pm
@@ -284,7 +284,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2008-2013, brian d foy, All Rights Reserved.
+Copyright © 2008-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
