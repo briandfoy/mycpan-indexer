39a3badbcf0e8a4d8d714235c91b5dec53b87edf
brian d foy <brian.d.foy@gmail.com>
2018-05-16 13:37:07 -0400

    Bump copyright year

--- lib/MyCPAN/Indexer/NullTester.pm
+++ lib/MyCPAN/Indexer/NullTester.pm
@@ -141,7 +141,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2008-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2008-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
--- lib/MyCPAN/Indexer/Queue/FromList.pm
+++ lib/MyCPAN/Indexer/Queue/FromList.pm
@@ -86,7 +86,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2010-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2010-2018, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the terms of the Artistic License 2.0.
 
